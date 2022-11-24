f6f1f115827a9155049780a2d23c6599da2f3834
Bruno Costa <brn.98@live.com>
2022-11-24 00:48:09 +0000

    Importando do banco e usando DAO

---
+++ dao/PessoaDAO.py
@@ -0,0 +1,25 @@
+from model.Pessoa import Pessoa 
+
+class PessoaDAO:
+  conexao = None
+  cursor = None
+
+  def __init__(self, con, cur):
+    self.conexao = con
+    self.cursor = cur
+
+  def getAll(self, orderByNome = True):
+    sql = "SELECT id, nome FROM pessoa "
+
+    try:
+      self.cursor.execute(sql)
+      resultado = self.cursor.fetchall()
+
+      pessoas = []
+      for linha in resultado:
+        pessoa = Pessoa(linha[0], linha[1])
+        pessoas.append(pessoa)
+
+      return pessoas
+    except Exception as e:
+      return e
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-#Importar nosso aquivo Pessoa.py do diretorio model
+#Importa nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
-#exemplos de uso
+#Exemplo de uso
 bruno = Pessoa(1, "Bruno Costa")
 print(bruno)
 
-#mostrando somento o nome
+#Quero mostrar só o nome
 print(bruno.nome)
+
+print("###################")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#instancia objeteo pessoaDao
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#quero carregar uma lista com o retorno do banco
+pessoas = pessoaDAO.getAll()
+for pessoa in pessoas:
+  print(pessoa)
